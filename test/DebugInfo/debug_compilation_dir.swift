f9e233d3104ff187b7ff2d644422084c5ba772b0
Pofat Tseng <tjazzter@gmail.com>
2022-01-05 01:20:35 +0000

    1. Forward args from driver to frontend 2. Modify test case, use swiftc_driver instead

--- test/DebugInfo/debug_compilation_dir.swift
+++ test/DebugInfo/debug_compilation_dir.swift
@@ -1,11 +1,7 @@
-// RUN: %empty-directory(%t)
-// RUN: mkdir -p %t
-// RUN: cd %t
-// RUN: cp %s .
-// RUN: %target-swift-frontend -g \
-// RUN:   -wmo -c -debug-compilation-dir /var/empty \
-// RUN:   debug_compilation_dir.swift -o - -emit-ir | %FileCheck %s
+// RUN: %target-swiftc_driver -g \
+// RUN:   -wmo -c -debug-compilation-dir /path/to \
+// RUN:   %s -o - -emit-ir | %FileCheck %s
 
 func foo() {}
 
-// CHECK: !DIFile(filename: "debug_compilation_dir.swift", directory: "/var/empty")
+// CHECK: !DIFile(filename: "{{.*}}/debug_compilation_dir.swift", directory: "/path/to")
